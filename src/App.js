dbcaeff6493bb246cce7a233cac5d36713c4ff1e
shigeya-dd <59518903+shigeya-dd@users.noreply.github.com>
2022-05-09 15:08:04 +0900

    Added DD sdk

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { datadogRum } from '@datadog/browser-rum';
+    
+datadogRum.init({
+    applicationId: '73bac5d1-4bf0-4c6e-9047-513273db31f0',
+    clientToken: 'pub895892b85eaf78bd6d8b29546f4ee9c3',
+    site: 'datadoghq.com',
+    service:'test',
+    // Specify a version number to identify the deployed version of your application in Datadog 
+    // version: '1.0.0',
+    sampleRate: 100,
+    trackInteractions: true,
+    defaultPrivacyLevel: 'mask-user-input'
+});
+    
+datadogRum.startSessionReplayRecording();
 function App() {
   return (
     <div className="App">
